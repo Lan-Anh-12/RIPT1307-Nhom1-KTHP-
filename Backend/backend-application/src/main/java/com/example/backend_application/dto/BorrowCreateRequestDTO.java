db146950d426f8900377f0451af5bd430e5e2697
LanAnh <tranlananh12bpt@gmail.com>
2026-05-30 16:20:52 +0700

    thêm Dto tạo api mới(mới tạo yêu cầu mượn

--- Backend/backend-application/src/main/java/com/example/backend_application/dto/BorrowCreateRequestDTO.java
+++ Backend/backend-application/src/main/java/com/example/backend_application/dto/BorrowCreateRequestDTO.java
@@ -1,11 +1,16 @@
 package com.example.backend_application.dto;
 
+import java.time.LocalDate;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class BorrowCreateRequestDTO {
-    
-    
+    private Long id;
+    private LocalDate requestDate;
+    private LocalDate expectedReturnDate;
+    private Long deviceItemId;
+    private Integer quantity;
 }
